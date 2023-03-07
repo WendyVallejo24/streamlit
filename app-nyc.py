df63d9686a109c934edf60d8b598ad6623a9de82
Wendy Vallejo <wbvalpat24@gmail.com>
2023-03-07 10:08:17 -0600

    update 2

--- app-nyc.py
+++ app-nyc.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-import codecs
 import numpy as np
 
 DATA_URL = 'citibike-tripdata.csv'
@@ -8,27 +7,35 @@ DATE_COLUMN = 'started_at'
 
 st.title('Cicle Ride in New York')
 st.sidebar.image('credencial.jpg')
+st.sidebar.write('Correo Electronico')
 st.sidebar.write('zS20006733@estudiantes.uv.mx')
 st.sidebar.write("Wendy Belén Vallejo Patraca")
 st.sidebar.write("S20006733")
 
 @st.cache
 def load_data(nrows):
-    DATA_URL = codecs.open('citibike-tripdata.csv','rU','latin1')
     data = pd.read_csv(DATA_URL, nrows=nrows)
+    data.rename({'start_lat': 'lat', 'start_lng': 'lon'}, axis=1, inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+
     return data
 
 data_load_state = st.text('Cargando...')
 data = load_data(500)
 data_load_state.text('Done! (using st.cache)')
 
-st.header("Dataset")
-agree = st.sidebar.checkbox("Show row data ")
-if agree:
-  st.dataframe(data)
+if st.sidebar.checkbox("Show row data "):
+  st.subheader('Raw Data')
+  st.write(data)
 
 if st.sidebar.checkbox('Recorridos por hora'):
     st.subheader('Histograma de Numero de recorridos por hora')
 
     hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
     st.bar_chart(hist_values)
+
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
